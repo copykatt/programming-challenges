94110b6313031eb9f4edef305db0b93328bf5ed0
copykatt <copykatt@8chan.co>
2015-01-31 03:47:10 +0100

    Implemented BigInteger for fact. 100

--- Problems 20-29/28 - Calculate & Print 100 Factorial/CalcAndPrintFactorial.java
+++ Problems 20-29/28 - Calculate & Print 100 Factorial/CalcAndPrintFactorial.java
@@ -1,20 +1,27 @@
 import java.math.BigInteger;
 
 public class CalcAndPrintFactorial {
-  BigInteger n = new BigInteger();
+  BigInteger n;
 
   public CalcAndPrintFactorial (BigInteger n) {
     this.n = n;
   }
 
   public void start () {
+    long startTime = System.currentTimeMillis();
+
     System.out.println(fact (n));
+
+    long endTime = System.currentTimeMillis();
+    long totalTime = endTime - startTime;
+
+    System.out.println("Fact. of " + n + "\nRuntime " + totalTime + "ms.");
   }
 
   public BigInteger fact (BigInteger n) {
-    if (n == 0) return 1;
+    if (n.equals(BigInteger.ONE)) return BigInteger.ONE;
     else 
-      return n * (fact(n - 1));
+      return n.multiply(fact(n.subtract(BigInteger.ONE)));
   }
 
 }
@@ -22,23 +29,20 @@ public class CalcAndPrintFactorial {
 class CalcAndPrintFactorialDemo {
 
   public static void main (String[] args) {
-    long startTime = System.currentTimeMillis();
-    int n;
-    n = Integer.parseInt(args[0]);
-    BigInteger bigint = new BigInteger(n); 
+        BigInteger biggy = new BigInteger (args[0]);
 
-    CalcAndPrintFactorial calcapfact = new CalcAndPrintFactorial (BigInteger(valueOf(bigint)));
+    CalcAndPrintFactorial calcapfact = new CalcAndPrintFactorial (biggy);
     
     calcapfact.start ();
 
-    long endTime = System.currentTimeMillis();
-    long totalTime = endTime - startTime;
-    System.out.println("Calculated factorial of " + n + " in " + totalTime + "ms.");
   }
 
 }
 
 /* OUTPUT
- *
+ * ╰─± java CalcAndPrintFactorialDemo 100
+ *  93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
+ *  Fact. of 100
+ *  Runtime: 8ms.
  *
  */
